14ba9bf0b569deb6ae1c7bb1b69d4a9e89e817e2
TymoteuszJG <tymec9@gmail.com>
2023-12-11 20:44:43 +0100

    SettingUp
    Setting up DataLibrary

---
+++ LibrarySystem/DataLibrary/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace DataLibrary
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+        }
+     
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
